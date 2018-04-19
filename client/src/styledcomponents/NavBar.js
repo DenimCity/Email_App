097d6793508618d5b41ea4d6b031f72ab458877d
DenimCity <altidorj1@gmail.com>
2018-04-18 22:32:38 -0400

    creating nav bar

---
+++ client/src/styledcomponents/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components'
+const NavBar = () => {
+  return (
+    <Container>
+    <div>
+      
+    </div>
+    </Container>
+  );
+};
+
+export default NavBar;
+
+const Container = styled.div`
+background-color:grey;
+border-bottom: 1px solid #EAEAEB;
+text-align:center;
+width:100%;
+min-height:200px;
+display:flex;
+justify-content:center;
+align-items:center; 
+flex-direction:column;
+
+`
