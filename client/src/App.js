bcd511eabe97dec614054a073b1961e7e743857e
DenimCity <altidorj1@gmail.com>
2018-04-19 11:51:21 -0400

    created routes and imported nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-
+import NavBar from './styledcomponents/NavBar'
+import  HomePage from './components/HomePage'
+import  Login from './components/Login'
+import  Register from './components/Register'
 class App extends Component {
   render() {
     return (
       <div>
+        <NavBar/>
         <Router>
           <Switch>
-            <Route exact path="/" component={Homepage}/>
+            <Route exact path="/" component={HomePage}/>
             <Route exact path="/register" component={Register}/>
             <Route exact path="/login" component={Login}/>
           </Switch>
