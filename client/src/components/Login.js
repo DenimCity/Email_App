aca583fbf90834ef04842838e1e715c115947de7
DenimCity <altidorj1@gmail.com>
2018-04-19 11:51:44 -0400

    created routes to pages

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,7 +4,18 @@ export default class Login extends Component {
   render() {
     return (
       <div>
-        
+        <div>hi from the login page</div>
+        <div>
+          <a href="">
+          <button>Submit</button>
+          </a>
+          
+          
+          <a href="/">
+          <button>Cancel</button>
+          </a>
+        </div>
+         
       </div>
     )
   }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -4,7 +4,17 @@ export default class Register extends Component {
   render() {
     return (
       <div>
-        
+        hi from the signup page
+        <div>
+          <a href="">
+          <button>Submit</button>
+          </a>
+          
+          
+          <a href="/">
+          <button>Cancel</button>
+          </a>
+        </div>
       </div>
     )
   }
