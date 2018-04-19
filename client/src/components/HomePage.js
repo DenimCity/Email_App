6745d9b9c4082aed18bfb6f8cdf8638faf83455b
DenimCity <altidorj1@gmail.com>
2018-04-19 11:51:44 -0400

    created routes to pages

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
-
+import React, {Component} from 'react'
+import styled from 'styled-components'
+import Login from './Login'
+import Register from './Register'
 export default class HomePage extends Component {
+
+  state = {}
+
   render() {
+
     return (
-      <div>
-        
-      </div>
+      <Container>
+
+        <div>
+          <a href="/login">
+            <button>Login</button>
+          </a>
+        </div>
+        <div>
+          <a href="/register">
+            <button>Sign Up</button>
+          </a>
+        </div>
+
+      </Container>
     )
   }
 }
+
+const Container = styled.div `
+display:flex;
+justify-content: space-evenly;
+align-content: center;
+padding:2vw;
+margin:2vw;
+`
